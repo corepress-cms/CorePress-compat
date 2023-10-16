9a9220dc5f08ee3727a399a1994b39d53d5a645f
Aaron Dewes <aaron.dewes@protonmail.com>
2023-10-16 18:11:12 +0000

    Move Services_JSON to the backwards compat plugin

---
+++ deprecated/admin.php
@@ -0,0 +1,4 @@
+<?php
+
+// Always load these
+require_once( __DIR__ . '/admin/functions.php' );
--- loader.php
+++ loader.php
@@ -1,12 +1,22 @@
 <?php
 
+// Always load these
 require_once( __DIR__ . '/deprecated/functions.php' );
 require_once( __DIR__ . '/deprecated/mu.php' );
 require_once( __DIR__ . '/deprecated/actions.php' );
 require_once( __DIR__ . '/deprecated/filters.php' );
 
+// This is called during a later part of the CorePress init process
 function backwards_compat_pluggable() {
     require_once( __DIR__ . '/deprecated/pluggable.php' );
 }
 
-require_once( __DIR__ . '/deprecated/admin/functions.php' );
+// This is only called if class-json.php is required
+function backwards_compat_json() {
+    require_once( __DIR__ . '/deprecated/json.php' );
+}
+
+// This is only called if the admin is loaded
+function backwards_compat_admin() {
+    require_once( __DIR__ . '/deprecated/admin.php' );
+}
