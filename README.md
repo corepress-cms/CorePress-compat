087823310b0af5b54f894990f723912c4b49065f
Aaron Dewes <aaron.dewes@protonmail.com>
2023-10-16 17:32:18 +0000

    Add private_to_published

---
+++ README.md
@@ -0,0 +1,7 @@
+# CorePress backwards compatibility
+
+This is a replacement for backwards compatibility features from Wordpress for CorePress.
+
+The code here is based on the original [WordPress](https://wordpress.org) code.
+
+For CorePress, we decided to move it into a plugin to increase modularity and avoid unnecessary bloat, but still allow users to use legacy plugins.
--- deprecated/actions.php
+++ deprecated/actions.php
@@ -15,3 +15,19 @@ function call_mispelled_retrieve_password($user_login) {
 }
 
 add_action( 'retreive_password', 'call_mispelled_retrieve_password' );
+
+/**
+ * @param WP_Post $post Post object.
+ */
+function call_deprecated_private_to_publish($post) {
+	/**
+	 * Fires when a post's status is transitioned from private to published.
+	 *
+	 * @since 1.5.0
+	 * @deprecated 2.3.0 Use {@see 'private_to_publish'} instead.
+	 *
+	 * @param int $post_id Post ID.
+	 */
+	do_action_deprecated( 'private_to_published', array( $post->ID ), '2.3.0', 'private_to_publish' );
+}
+add_action( 'private_to_publish', 'call_deprecated_private_to_publish' );
